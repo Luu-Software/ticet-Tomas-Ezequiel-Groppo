676bb029f720c9f309e07d327220f7e8f14690eb
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-09 15:57:15 -0300

    Consigna base terminada

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,56 @@ DARIO       |       50%
 
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
 
+function calcularTotal(id:string, cantidad:number, codigo:string){
+  
+  let precioND: number; 
+  let artista: number;
+  
+  if (id=== "sabrina"){
+    artista=1000
+  }
+  else if (id==="kgatlw"){
+   artista=700
+  }
+  else if (id==="lali"){
+   artista=500
+  }
+  else if (id==="magdalena"){
+    artista=600
+  }
+  else if (id==="viagra"){
+    artista=400
+  }
+  else if (id==="dillom"){
+    artista=350
+  }
+  else if (id==="marilina"){
+    artista=200
+  }
+  else if(id==="mugre"){
+     artista=150
+  }
+  else{
+     artista=0;
+  }
+   precioND= artista*cantidad
+  
+  if (codigo==="TIC10"){
+    return precioND * (9/10);
+  }
+  else if (codigo==="TIC20"){
+    return precioND * (4/5);
+  }
+  else if (codigo="DARIO"){
+    return precioND * (1/2);
+  }
+  else{
+    return precioND;
+  }
+
+  
+}
+
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
   let cantidadNum: number = Number(cantidad);
   let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
