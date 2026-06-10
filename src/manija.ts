68214127e78a812bfd43105f2c3e8009cf19f5ed
GROPPO TOMAS EZEQUIEL <50976178@ort>
2026-06-10 14:29:11 -0300

    Intente hacer algo, no pude (En la manija)

--- src/manija.ts
+++ src/manija.ts
@@ -1,5 +1,10 @@
 export function generarLorem(n: number): string {
-  // COMPLETAR: Implementar la función generarLorem que reciba un número n y devuelva una cadena de texto con la frase "Lorem Ipsum" repetida n veces, separada por espacios. Por ejemplo, si n es 3, la función debería devolver "Lorem Ipsum Lorem Ipsum Lorem Ipsum".
+  if (n===0){
+    return "."
+  }
+  else{
+    return "Lorem ipsum"
+  }
 }
 
 console.log(generarLorem(5));
